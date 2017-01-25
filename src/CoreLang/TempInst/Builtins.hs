f168d4c7481e1a18d9f4e2f9395b9c571b2d2775
Martin Huschenbett <martin.huschenbett@posteo.me>
2017-01-25 01:10:16 +0000

    Introduced helper function for builtins.

--- src/CoreLang/TempInst/Builtins.hs
+++ src/CoreLang/TempInst/Builtins.hs
@@ -27,43 +27,37 @@ everything =
   ]
 
 
-prim1 :: (Node -> TIM Node) -> TIM ()
-prim1 impl = do
+withDataArg :: (Node -> TIM ()) -> TIM ()
+withDataArg cont = do
   pop
   appAddr <- top
   Application _ argAddr <- deref appAddr
   argNode <- deref argAddr
   if isDataNode argNode then do
-    resNode <- impl argNode
-    update appAddr resNode
+    cont argNode
   else do
     dump
     push argAddr
+
+updateTopNode :: Node -> TIM ()
+updateTopNode node = do
+  addr <- top
+  update addr node
+
+prim1 :: (Node -> TIM Node) -> TIM ()
+prim1 impl =
+  withDataArg $ \argNode ->
+    impl argNode >>= updateTopNode
 
 arith1 :: (Integer -> Integer) -> TIM ()
 arith1 op = prim1 $ \node -> mkNumber <$> op <$> getNumber node
 
 
 prim2 :: (Node -> Node -> TIM Node) -> TIM ()
-prim2 impl = do
-  pop
-  app1Addr <- top
-  Application _ arg1Addr <- deref app1Addr
-  arg1Node <- deref arg1Addr
-  if isDataNode arg1Node then do
-    pop
-    app2Addr <- top
-    Application _ arg2Addr <- deref app2Addr
-    arg2Node <- deref arg2Addr
-    if isDataNode arg2Node then do
-      resNode <- impl arg1Node arg2Node
-      update app2Addr resNode
-    else do
-      dump
-      push arg2Addr
-  else do
-    dump
-    push arg1Addr
+prim2 impl = 
+  withDataArg $ \arg1Node -> do
+    withDataArg $ \arg2Node -> do
+      impl arg1Node arg2Node >>= updateTopNode
 
 arith2 :: (Integer -> Integer -> Integer) -> TIM ()
 arith2 op = prim2 $ \node1 node2 ->
@@ -75,89 +69,62 @@ rel2 cmp = prim2 $ \node1 node2 ->
 
 
 if_ :: TIM ()
-if_ = do
-  pop
-  condAppAddr <- top
-  Application _ condAddr <- deref condAppAddr
-  condNode <- deref condAddr
-  if isDataNode condNode then do
-    pop -- condAppAddr
+if_ =
+  withDataArg $ \condNode -> do
+    pop
     thenAppAddr <- top
     pop
     elseAppAddr <- top
-    let rootAddr = elseAppAddr
     cond <- getBool condNode
     if cond then do
       Application _ thenAddr <- deref thenAppAddr
-      update rootAddr (Indirection thenAddr)
+      updateTopNode (Indirection thenAddr)
     else do
       Application _ elseAddr <- deref elseAppAddr
-      update rootAddr (Indirection elseAddr)
-  else do
-    dump
-    push condAddr
+      updateTopNode (Indirection elseAddr)
 
 casePair :: TIM ()
-casePair = do
-  pop
-  pairAppAddr <- top
-  Application _ pairAddr <- deref pairAppAddr
-  pairNode <- deref pairAddr
-  if isDataNode pairNode then do
-    pop -- pairAppAddr
-    funAppAddr <- top -- rootAddr
-    Data 0 [arg1Addr, arg2Addr] <- return pairNode
-    Application _ funAddr <- deref funAppAddr
-    arg1AppAddr <- alloc (Application funAddr arg1Addr)
-    update funAppAddr (Application arg1AppAddr arg2Addr)
-  else do
-    dump
-    push pairAddr
+casePair =
+  withDataArg $ \pairNode -> do
+    pop
+    funAppAddr <- top
+    case pairNode of
+      Data 0 [arg1Addr, arg2Addr] -> do
+        Application _ funAddr <- deref funAppAddr
+        arg1AppAddr <- alloc (Application funAddr arg1Addr)
+        updateTopNode (Application arg1AppAddr arg2Addr)
+      _ -> throwError "pattern match failed on case_pair"
 
 caseList :: TIM ()
-caseList = do
-  pop
-  listAppAddr <- top
-  Application _ listAddr <- deref listAppAddr
-  listNode <- deref listAddr
-  if isDataNode listNode then do
-    pop -- listAppAddr
+caseList =
+  withDataArg $ \listNode -> do
+    pop
     baseAppAddr <- top
     pop
-    stepAppAddr <- top -- rootAddr
+    stepAppAddr <- top
     Data t argAddrs <- return listNode
     case (t, argAddrs) of
       (0, []) -> do
         Application _ baseAddr <- deref baseAppAddr
-        update stepAppAddr (Indirection baseAddr)
+        updateTopNode (Indirection baseAddr)
       (1, [arg1Addr, arg2Addr]) -> do
         Application _ stepAddr <- deref stepAppAddr
         arg1AppAddr <- alloc (Application stepAddr arg1Addr)
-        update stepAppAddr (Application arg1AppAddr arg2Addr)
-      _ -> throwError "pattern match failed in stepUnlist"
-  else do
-    dump
-    push listAddr
+        updateTopNode (Application arg1AppAddr arg2Addr)
+      _ -> throwError "pattern match failed in case_list"
 
 
 printNum :: TIM ()
 printNum = do
   no_dump <- dumpNull
   if no_dump then do
-    pop
-    outAppAddr <- top
-    Application _ outAddr <- deref outAppAddr
-    outNode <- deref outAddr
-    if isDataNode outNode then do
+    withDataArg $ \outNode -> do
+      pop
       num <- getNumber outNode
       output (show num)
-      pop -- outAppAddr
-      retAppAddr <- top -- rootAddr
+      retAppAddr <- top
       Application _ retAddr <- deref retAppAddr
-      update retAppAddr (Indirection retAddr)
-    else do
-      dump
-      push outAddr
+      updateTopNode (Indirection retAddr)
   else
     throwError "dump not empty"
 
