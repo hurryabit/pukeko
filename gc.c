c21171fcb05379c838002dfc3433daaf59aab0ec
Martin Huschenbett <martin.huschenbett@posteo.me>
2017-04-16 16:24:15 +0100

    Allow heap cells of arbitrary size.

--- gc.c
+++ gc.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define TAG_NIX 0
 #define TAG_FWD 1
@@ -13,12 +14,14 @@
 #define TAG_MIN_GOOD TAG_APP
 #define TAG_MAX_GOOD TAG_FUN
 
+#define LIM_CON 4
+
 typedef struct heap_cell {
   uint8_t tag;
-  uint8_t extra_tag;
-  uint8_t unused[6];
-  struct heap_cell* dat1;
-  struct heap_cell* dat2;
+  uint8_t arity;
+  uint8_t size;
+  uint8_t unused[5];
+  struct heap_cell* data[0];
 } heap_cell;
 
 typedef heap_cell* stack_cell;
@@ -35,7 +38,7 @@ typedef struct {
   heap_cell*  heap_end;
   heap_cell*  heap_ptr;
   heap_cell*  heap_limit;
-  uint64_t    num_allocs;
+  uint64_t    alloc_bytes;
   uint64_t    num_gc_runs;
 } gc_info;
 
@@ -45,16 +48,16 @@ heap_cell* from_end;
 void gc_init(gc_info* info) {
   uint64_t half_heap_cells = info->heap_size / 48;
   info->heap_size   = 48 * half_heap_cells;
-  info->heap_middle = info->heap_start  + half_heap_cells;
-  info->heap_end    = info->heap_middle + half_heap_cells;
+  info->heap_middle = info->heap_start  + 3 * half_heap_cells;
+  info->heap_end    = info->heap_middle + 3 * half_heap_cells;
   info->heap_ptr    = info->heap_start;
   info->heap_limit  = info->heap_middle;
-  info->num_allocs -= (uint64_t) info->heap_ptr;
+  info->alloc_bytes -= (uint64_t) info->heap_ptr;
 }
 
 void gc_print_cell(heap_cell* ptr) {
   uint64_t* raw_ptr = (uint64_t*) ptr;
-  printf("<%llu(%u) %llu %llu> @ %llu",
+  printf("<0x%016llx(%u) 0x%016llx %016llx> @ 0x%016llx",
          *raw_ptr, ptr->tag, *(raw_ptr+1), *(raw_ptr+2), (uint64_t) raw_ptr);
 }
 
@@ -62,7 +65,7 @@ void gc_assert_good_tag(heap_cell* ptr, char* caller, bool fwd_ok) {
   if (ptr->tag == TAG_NIX
       || (fwd_ok && ptr->tag == TAG_FWD)
       || (TAG_MIN_GOOD <= ptr->tag && ptr->tag <= TAG_MAX_GOOD)
-      || ptr->tag >= TAG_CON)
+      || (TAG_CON <= ptr->tag && ptr->tag < TAG_CON + LIM_CON))
     return;
 
   printf("Found bad cell in %s: ", caller);
@@ -71,35 +74,35 @@ void gc_assert_good_tag(heap_cell* ptr, char* caller, bool fwd_ok) {
   exit(1);
 }
 
-// If ptr doesn't point into the heap, we return it unchanged. In particular, we
-// return NULL unchanged.
 heap_cell* gc_copy(gc_info* info, heap_cell* ptr) {
   if (ptr == NULL) {
-    return NULL;
+    puts("NULL POINTER EXCEPTION");
+    exit(1);
   }
 
   heap_cell* ptr0 = ptr;
   uint64_t fwd_chain = 0;
   while (ptr->tag == TAG_FWD) {
-    ptr = ptr->dat1;
+    ptr = ptr->data[0];
   }
   gc_assert_good_tag(ptr, "gc_copy", false);
 
   if (from_start <= ptr && ptr < from_end) {
     heap_cell* copy_ptr = info->heap_ptr;
-    info->heap_ptr += 1;
+    info->heap_ptr += ptr->size;
 
-    *copy_ptr = *ptr;
+    // TODO: A handwritten loop might be faster since everything is aligned
+    // neatly.
+    memcpy(copy_ptr, ptr, 8*ptr->size);
     ptr->tag = TAG_FWD;
-    ptr->extra_tag = 0;
-    ptr->dat1 = copy_ptr;
-    ptr->dat2 = NULL;
+    ptr->arity = 0;
+    ptr->data[0] = copy_ptr;
     ptr = copy_ptr;
   }
 
   while (ptr0->tag == TAG_FWD) {
-    heap_cell* ptr1 = ptr0->dat1;
-    ptr0->dat1 = ptr;
+    heap_cell* ptr1 = ptr0->data[0];
+    ptr0->data[0] = ptr;
     ptr0 = ptr1;
   }
 
@@ -108,12 +111,18 @@ heap_cell* gc_copy(gc_info* info, heap_cell* ptr) {
 
 void gc_follow(gc_info* info, heap_cell*  ptr) {
   if (ptr->tag == TAG_FWD) {
-    ptr->dat1 = gc_copy(info, ptr->dat1);
+    ptr->data[0] = gc_copy(info, ptr->data[0]);
   }
 
-  if (ptr->tag == TAG_APP || ptr->tag >= TAG_CON) {
-    ptr->dat1 = gc_copy(info, ptr->dat1);
-    ptr->dat2 = gc_copy(info, ptr->dat2);
+  if (ptr->tag == TAG_APP) {
+    ptr->data[0] = gc_copy(info, ptr->data[0]);
+    ptr->data[1] = gc_copy(info, ptr->data[1]);
+  }
+
+  if (ptr->tag >= TAG_CON) {
+    for (uint8_t i = 0; i < ptr->arity; ++i) {
+      ptr->data[i] = gc_copy(info, ptr->data[i]);
+    }
   }
 }
 
@@ -123,8 +132,13 @@ void gc_stack_frame(gc_info* info, stack_cell* frame_top, stack_cell* frame_base
 }
 
 void gc_collect(gc_info* info, uint64_t heap_claim) {
+  /* puts("entering gc"); */
+  if (info->heap_ptr > info->heap_limit) {
+    puts("OVERFILLED HEAP:");
+    exit(1);
+  }
   info->num_gc_runs += 1;
-  info->num_allocs += (uint64_t) info->heap_ptr;
+  info->alloc_bytes += (uint64_t) info->heap_ptr;
 
   if (info->heap_ptr <= info->heap_middle) {
     info->heap_ptr   = info->heap_middle;
@@ -140,7 +154,7 @@ void gc_collect(gc_info* info, uint64_t heap_claim) {
   }
   heap_cell* follow_ptr = info->heap_ptr;
 
-  for (heap_cell* caf_ptr = info->cafs_start; caf_ptr < info->cafs_end; ++caf_ptr) {
+  for (heap_cell* caf_ptr = info->cafs_start; caf_ptr < info->cafs_end; caf_ptr += caf_ptr->size) {
     gc_assert_good_tag(caf_ptr, "gc_follow@cafs", true);
     gc_follow(info, caf_ptr);
   }
@@ -158,7 +172,7 @@ void gc_collect(gc_info* info, uint64_t heap_claim) {
   while (follow_ptr < info->heap_ptr) {
     gc_assert_good_tag(follow_ptr, "gc_follow@finish", false);
     gc_follow(info, follow_ptr);
-    ++follow_ptr;
+    follow_ptr += follow_ptr->size;
   }
 
   if (info->heap_ptr + heap_claim > info->heap_limit) {
@@ -166,15 +180,16 @@ void gc_collect(gc_info* info, uint64_t heap_claim) {
     exit(1);
   }
 
-  info->num_allocs -= (uint64_t) info->heap_ptr;
+  info->alloc_bytes -= (uint64_t) info->heap_ptr;
+  /* puts("leaving gc"); */
 }
 
 void gc_stats(gc_info* info, uint64_t steps, uint64_t checks) {
-  info->num_allocs += (uint64_t) info->heap_ptr;
+  info->alloc_bytes += (uint64_t) info->heap_ptr;
 
   printf("\n");
-  printf("Steps  : %12llu\n", steps);
-  printf("Allocs : %12llu          (Checks: %12llu)\n",
-         info->num_allocs / 24, checks);
-  printf("GC runs: %12llu\n", info->num_gc_runs);
+  printf("Steps      : %12llu\n", steps);
+  printf("Alloc bytes: %12llu          (Checks: %12llu)\n",
+         info->alloc_bytes, checks);
+  printf("GC runs    : %12llu\n", info->num_gc_runs);
 }
