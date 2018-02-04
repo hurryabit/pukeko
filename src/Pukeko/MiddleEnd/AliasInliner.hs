96e55537cd28b2a228f0d53919b1eae50a952e58
Martin Huschenbett <martin.huschenbett@posteo.me>
2018-02-04 10:37:03 +0100

    Use union-find library in alias inliner

--- src/Pukeko/MiddleEnd/AliasInliner.hs
+++ src/Pukeko/MiddleEnd/AliasInliner.hs
@@ -1,26 +1,47 @@
-module Pukeko.MiddleEnd.AliasInliner
-  ( inlineModule
-  ) where
+-- | Perform link compression for functions which are just aliases. For instance,
+--
+-- > f = Cons 1 Nil
+-- > g = f
+-- > h = g
+-- > k = append g h
+--
+-- is transformed into
+--
+-- > f = Cons 1 Nil
+-- > g = f
+-- > h = f
+-- > k = append f f
+--
+-- Notice that @g@ and @h@ are dead code now.
+module Pukeko.MiddleEnd.AliasInliner where
 
-import Pukeko.Prelude hiding (join)
+import Pukeko.Prelude
 
 import           Control.Monad.ST
-import qualified Data.Array        as A
-import qualified Data.Array.ST     as A
 import qualified Data.Map          as Map
 import qualified Data.Set          as Set
+import qualified Data.UnionFind.ST as UF
 
 import           Pukeko.AST.SystemF
 import qualified Pukeko.AST.Identifier as Id
 
+-- | Follow all chains of links in a module and adjust all call sites accordingly.
 inlineModule :: Module st -> Module st
-inlineModule (MkModule decls0) =
+inlineModule = over module2decls inlineDecls
+
+-- | Follow all chains of links in a group of declarations and adjust all call
+-- sites within this group.
+inlineDecls :: [Decl st] -> [Decl st]
+inlineDecls decls0 =
   let ls = mapMaybe declLink decls0
       uf = unionFind ls
-      decls1 = over (traverse . decl2atom . _AVal)
-               (\x -> Map.findWithDefault x x uf) decls0
-  in  MkModule decls1
+  in  over (traverse . decl2atom . _AVal) (\x -> Map.findWithDefault x x uf) decls0
 
+-- | Determine if a declaration is a link, i.e., of the form
+--
+-- > f = g
+--
+-- If it is, return the pair @(f, g)@.
 declLink :: Decl st -> Maybe (Id.EVar, Id.EVar)
 declLink = \case
   DDefn (MkDefn b (EVal x)) -> Just (b^.bind2evar.lctd, x)
@@ -28,43 +49,11 @@ declLink = \case
     | null vs && null xs -> Just (z^.lctd, x)
   _ -> Nothing
 
-data UnionFind st a = UnionFind
-  { indices :: Map a Int
-  , names   :: A.Array Int a
-  , links   :: A.STArray st Int Int
-  }
-
-newUnionFind :: Ord a => Set a -> ST s (UnionFind s a)
-newUnionFind xs = do
-  let is = Map.fromList (zip (toList xs) [0..])
-  let bnds = (0, Map.size is - 1)
-  let ns = A.array bnds (map (\(x, i) -> (i, x)) (Map.toList is))
-  ls <- A.newListArray bnds [0..]
-  pure (UnionFind is ns ls)
-
-root :: UnionFind s a -> Int -> ST s Int
-root uf i = do
-  j <- A.readArray (links uf) i
-  if i /= j
-    then do
-      k <- A.readArray (links uf) j
-      A.writeArray (links uf) i k
-      root uf j
-    else
-      pure i
-
-join' :: UnionFind s a -> Int -> Int -> ST s ()
-join' uf i j = do
-  l <- root uf j
-  A.writeArray (links uf) i l
-
-join :: Ord a => UnionFind s a -> a -> a -> ST s ()
-join uf x y = join' uf (indices uf ! x) (indices uf ! y)
-  where (!) = flip (Map.findWithDefault (bug "join"))
-
+-- | Run Tarjan's union find algorithm on a list of equivalences and return a
+-- map from each element to its representative.
 unionFind :: Ord a => [(a, a)] -> Map a a
 unionFind xys = runST $ do
   let xs = foldMap (\(x, y) -> Set.singleton x <> Set.singleton y) xys
-  uf <- newUnionFind xs
-  for_ xys (uncurry (join uf))
-  traverse (\i -> (names uf A.!) <$> root uf i) (indices uf)
+  ps <- sequence (Map.fromSet UF.fresh xs)
+  for_ xys $ \(x, y) -> UF.union (ps Map.! x) (ps Map.! y)
+  for ps $ \p -> UF.repr p >>= UF.descriptor
