d44a9efbf9316a422145c3add4bada2b6134fe74
Martin Huschenbett <martin.huschenbett@posteo.me>
2018-03-09 21:53:30 +0100

    Fix bug in eta reducer
    Currently, the eta reducer would wrongly reduce
    
      f : Int -> Int
      f x = x + x
    
    to
    
      f : Int -> Int
      f = (+) x
    
    This commit fixes this issue.

--- test/lambdalift.pl
+++ test/lambdalift.pl
@@ -32,9 +32,24 @@ data Monad m =
 data World =
        | World
 data IO a = World -> Pair a World
+external neg_int : Int -> Int = "neg"
+external add_int : Int -> Int -> Int = "add"
+external sub_int : Int -> Int -> Int = "sub"
+external mul_int : Int -> Int -> Int = "mul"
 external seq : ∀a b. a -> b -> b = "seq"
 external puti : Int -> Unit = "puti"
-main : IO Unit = print$ll1 (g$ll1 1 2 3 4)
+dict$Ring$Int : Ring Int =
+  let neg : Int -> Int = neg_int
+  and add : Int -> Int -> Int = add_int
+  and sub : Int -> Int -> Int = sub_int
+  and mul : Int -> Int -> Int = mul_int
+  in
+  Dict$Ring @Int neg add sub mul
+main : IO Unit = print$ll1 (g$ll1 1 2 3 (h$ll1 2))
+add$ll1 : ∀a. Ring a -> a -> a -> a =
+  fun @a (dict : Ring a) ->
+    match dict with
+    | Dict$Ring @a _ add _ _ -> add
 io$ll1 : ∀a b. (a -> b) -> a -> World -> Pair b World =
   fun @a @b (f : a -> b) (x : a) (world : World) ->
     let y : b = f x in
@@ -45,3 +60,5 @@ io$ll2 : ∀a b. (a -> b) -> a -> IO b =
 print$ll1 : Int -> IO Unit = io$ll2 @Int @Unit puti
 g$ll1 : Int -> Int -> Int -> Int -> Int =
   fun (a : Int) (b : Int) (c : Int) (d : Int) -> a
+h$ll1 : Int -> Int =
+  fun (x : Int) -> add$ll1 @Int dict$Ring$Int x x
