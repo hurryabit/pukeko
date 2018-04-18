374f48f05b3dc76f2f456f99d5c3a887f92f80a3
Martin Huschenbett <martin.huschenbett@posteo.me>
2018-04-19 01:19:28 +0200

    Prepare representation of contexts for super classes (#29)

--- src/Pukeko/FrontEnd/Inferencer/Gamma.hs
+++ src/Pukeko/FrontEnd/Inferencer/Gamma.hs
@@ -23,7 +23,6 @@ module Pukeko.FrontEnd.Inferencer.Gamma
 import Pukeko.Prelude
 
 import qualified Data.Map.Extended as Map
-import qualified Data.Set          as Set
 
 import           Pukeko.AST.Dict (DxBinder)
 import           Pukeko.AST.Expr (TmBinder)
@@ -33,15 +32,14 @@ import           Pukeko.FrontEnd.Inferencer.UType
 data Gamma s = Gamma
   { _tmVars  :: Map TmVar (UType s)
   , _level   :: Level
-  , _tyVars  :: Set TyVar
-  , _context :: Map (Class, TyVar) DxVar
+  , _tyVars  :: Map TyVar (Map Class DxVar)
   }
 makeLenses ''Gamma
 
 type CanGamma s effs = Member (Reader (Gamma s)) effs
 
 runGamma :: Eff (Reader (Gamma s) : effs) a -> Eff effs a
-runGamma = runReader (Gamma Map.empty topLevel Set.empty Map.empty)
+runGamma = runReader (Gamma Map.empty topLevel Map.empty)
 
 introTmVar :: CanGamma s effs => TmBinder (UType s) -> Eff effs a -> Eff effs a
 introTmVar (x, t) = locally tmVars (Map.insertWith impossible x t)
@@ -59,27 +57,27 @@ getLevel :: forall s effs. CanGamma s effs => Eff effs Level
 getLevel = view (level @s)
 
 introTyVar :: forall s effs a. CanGamma s effs => TyVar -> Eff effs a -> Eff effs a
-introTyVar v act = do
-  views (tyVars @s) (Set.notMember v) >>= assertM
-  locally (tyVars @s) (Set.insert v) act
+introTyVar v = locally (tyVars @s) (Map.insertWith impossible v Map.empty)
 
 introTyVars :: forall s effs a. CanGamma s effs => [TyVar] -> Eff effs a -> Eff effs a
 introTyVars = flip (foldr (introTyVar @s))
 
 checkTyVar :: forall s effs. CanGamma s effs => TyVar -> Eff effs ()
-checkTyVar v = views (tyVars @s) (Set.member v) >>= assertM
+checkTyVar v = views (tyVars @s) (Map.member v) >>= assertM
 
 enterContext1 :: forall s effs a. CanGamma s effs =>
   DxBinder (UType s) -> Eff effs a -> Eff effs a
-enterContext1 (dvar, (clss, UTVar tvar)) act = do
-  checkTyVar @s tvar
-  locally (context @s) (Map.insertWith impossible (clss, tvar) dvar) act
-enterContext1 _ _ = impossible
+enterContext1 (dvar, (clss, UTVar tvar)) = locally (tyVars @s) (Map.alter f tvar)
+  where
+    f = \case
+      Nothing -> impossible
+      Just dvars -> Just (Map.insertWith impossible clss dvar dvars)
+enterContext1 _ = impossible
 
 enterContext :: forall s effs a. CanGamma s effs =>
   [DxBinder (UType s)] -> Eff effs a -> Eff effs a
 enterContext = flip (foldr enterContext1)
 
 lookupContext :: forall s effs. CanGamma s effs =>
-  (Class, TyVar)-> Eff effs (Maybe DxVar)
-lookupContext v = views (context @s) (Map.lookup v)
+  TyVar-> Eff effs (Maybe (Map Class DxVar))
+lookupContext v = views (tyVars @s) (Map.lookup v)
